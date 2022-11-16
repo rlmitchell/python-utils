89c02c8caeed7cdfbfeed0e3ed6c97a1c7daef8b
Rob Mitchell <rlmitchell@gmail.com>
2022-11-15 20:54:31 -0600

    rds_manual_snapshots_list.py

--- aws/rds_manual_snapshots_list.py
+++ aws/rds_manual_snapshots_list.py
@@ -8,12 +8,11 @@ __updated__    = '2022.11.12.1317'
 
 import boto3
 from aws.rds_client import RDSClient
-from pprint import pprint
 
 
 class AWSRDSManualSnapshotsList:
-    def __init__(self, db_identifier, access_key=None, secret_key=None, region=None):
-        self.client = RDSClient(db_identifier=db_identifier, access_key=access_key, secret_key=secret_key, region=region)
+    def __init__(self, db_identifier, aws_credentials={}):
+        self.client = RDSClient(db_identifier=db_identifier, aws_credentials=aws_credentials)
 
     def __call__(self):
         return self.client.get_manual_snapshots()
